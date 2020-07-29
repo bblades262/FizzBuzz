47e8e3a8e953af63980a741a56fe58d378a2823f
finn campbell <finnfinn11181118@gmail.com>
2020-07-29 15:59:53 -0400

    added user input to determine numbers labeled as "fizz" or "buzz"

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,11 @@
-
-
-for i in range(1, 101):
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz")
-    elif i % 3 == 0:
-        print("Fizz")
-    elif i % 5 == 0:
-        print("Buzz")
+dividerone = int(input("what should the first divider be?: "))
+dividertwo = int(input("what should the second divider be?: "))
+for i in range(1,101):
+    if i%(dividerone) ==0 and i%(dividertwo) ==0:
+        print("fizzbuzz")
+    elif i%(dividerone) ==0:
+        print("fizz")
+    elif i%(dividertwo) ==0:
+        print("buzz")
     else:
         print(i)
