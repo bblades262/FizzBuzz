722dbb9a90a5766ac27ff8ebe72df0a9a74dbf61
finn campbell <finnfinn11181118@gmail.com>
2020-07-29 16:33:32 -0400

    added "fizzbuzz" function to determine the label of an inputted number

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,4 @@
+"""
 dividerone = int(input("what should the first divider be?: "))
 dividertwo = int(input("what should the second divider be?: "))
 for i in range(1,101):
@@ -6,6 +7,17 @@ for i in range(1,101):
     elif i%(dividerone) ==0:
         print("fizz")
     elif i%(dividertwo) ==0:
-        print("buzz")
     else:
-        print(i)
+       print(i)
+"""
+
+
+def fizzbuzz(inputnumber):
+    if inputnumber % 3 == 0 and inputnumber % 5 == 0:
+        return "fizzbuzz"
+    elif inputnumber % 3 == 0:
+        return "fizz"
+    elif inputnumber % 5 == 0:
+        return "buzz"
+    else:
+        return inputnumber
