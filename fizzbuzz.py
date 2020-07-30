459385e054099cc669e8dee7e625d38f92870965
bblades <bblades262@gmail.com>
2020-07-30 15:55:17 -0400

    changed all buttons from relative size to fixed size. also adjusted main window size to be more asthetically pleasing

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 root = Tk()
 root.title("calculator")
-root.geometry("410x300")
+root.geometry("460x300")
 
 e = Entry(root)
 e.grid(row=4, column=5)
@@ -53,26 +53,26 @@ def button_divide():
     e.delete(0, END)
 
 
-zerobutton = Button(root, text="0", padx=25, pady=25,command=lambda: button_click(0)).grid(row=4, column=2)
-onebutton = Button(root, text="1", padx=25, pady=25, command=lambda: button_click(1)).grid(row=1, column=1)
-twobutton = Button(root, text="2", padx=25, pady=25, command=lambda: button_click(2)).grid(row=1, column=2)
-threebutton = Button(root, text="3", padx=25, pady=25, command=lambda: button_click(3)).grid(row=1, column=3)
-fourbutton = Button(root, text="4", padx=25, pady=25, command=lambda: button_click(4)).grid(row=2, column=1)
-fivebutton = Button(root, text="5", padx=25, pady=25, command=lambda: button_click(5)).grid(row=2, column=2)
-sixbutton = Button(root, text="6", padx=25, pady=25, command=lambda: button_click(6)).grid(row=2, column=3)
-sevenbutton = Button(root, text="7", padx=25, pady=25, command=lambda: button_click(7)).grid(row=3, column=1)
-eightbutton = Button(root, text="8", padx=25, pady=25, command=lambda: button_click(8)).grid(row=3, column=2)
-ninebutton = Button(root, text="9", padx=25, pady=25, command=lambda: button_click(9)).grid(row=3, column=3)
+zerobutton = Button(root, text="0", height=3, width=5, command=lambda: button_click(0)).grid(row=4, column=2)
+onebutton = Button(root, text="1", height=3, width=5, command=lambda: button_click(1)).grid(row=1, column=1)
+twobutton = Button(root, text="2", height=3, width=5, command=lambda: button_click(2)).grid(row=1, column=2)
+threebutton = Button(root, text="3", height=3, width=5, command=lambda: button_click(3)).grid(row=1, column=3)
+fourbutton = Button(root, text="4", height=3, width=5, command=lambda: button_click(4)).grid(row=2, column=1)
+fivebutton = Button(root, text="5", height=3, width=5, command=lambda: button_click(5)).grid(row=2, column=2)
+sixbutton = Button(root, text="6", height=3, width=5, command=lambda: button_click(6)).grid(row=2, column=3)
+sevenbutton = Button(root, text="7", height=3, width=5, command=lambda: button_click(7)).grid(row=3, column=1)
+eightbutton = Button(root, text="8", height=3, width=5, command=lambda: button_click(8)).grid(row=3, column=2)
+ninebutton = Button(root, text="9", height=3, width=5, command=lambda: button_click(9)).grid(row=3, column=3)
 
 
-clearbutton = Button(root, text="C", padx=25, pady=25, command=button_clear).grid(row=4, column=1)
-equalbutton = Button(root, text="=", padx=25, pady=25, command=button_equal).grid(row=4, column=3)
+clearbutton = Button(root, text="C", height=3, width=5, command=button_clear).grid(row=4, column=1)
+equalbutton = Button(root, text="=", height=3, width=5, command=button_equal).grid(row=4, column=3)
 
 
-plusbutton = Button(root, text="+", padx=25, pady=25, command=button_add).grid(row=1, column=4)
-minusbutton = Button(root, text="-", padx=25, pady=25, command=button_subtract).grid(row=2, column=4)
-miltiplybutton = Button(root, text="x", padx=25, pady=25, command=button_multiply).grid(row=3, column=4)
-dividebutton = Button(root, text="/", padx=25, pady=25, command=button_divide).grid(row=4, column=4)
+plusbutton = Button(root, text="+", height=3, width=5, command=button_add).grid(row=1, column=4)
+minusbutton = Button(root, text="-", height=3, width=5, command=button_subtract).grid(row=2, column=4)
+miltiplybutton = Button(root, text="x", height=3, width=5, command=button_multiply).grid(row=3, column=4)
+dividebutton = Button(root, text="/", height=3, width=5, command=button_divide).grid(row=4, column=4)
 
 
 root.mainloop()
