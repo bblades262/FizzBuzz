86ed72aa1fa8dde5c9939f6734006b0029a44342
bblades <bblades262@gmail.com>
2020-07-31 11:52:33 -0400

    created new file calculator.py and moved calc code into that. restored fizzbuzz.py to its former glory.

---
+++ venv/fizzbuzz.py
@@ -0,0 +1,9 @@
+def fizzbuzz(myinputnumber):
+    if myinputnumber % 3 == 0 and myinputnumber % 5 == 0:
+        return "fizzbuzz"
+    if myinputnumber % 3 == 0:
+        return "fizz"
+    if myinputnumber % 3 == 0:
+        return "buzz"
+    else:
+        return myinputnumber
